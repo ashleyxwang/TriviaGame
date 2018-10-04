408720a3208f6dab2540591f83abe03a1cf63199
ashleyxwang <ashleywang514@yahoo.com>
2018-10-03 23:57:54 -0500

    fix timer and repeating questions on reset

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 var instructionsContent = "Three questions, 30 seconds...";
 var timer = 0;
-var countDown = 5;
+var countDown = 10;
 var numCorrect = 0;
 var numIncorrect = 0;
 var numUnanswered = 0;
@@ -15,7 +15,7 @@ var questions = [
         text: "What color is the grass?",
         choices: ["red", "yellow", "blue", "green"],
         correctIndex: 3,
-    },    
+    },
     {
         text: "What color is the sun?",
         choices: ["red", "yellow", "blue", "green"],
@@ -23,77 +23,81 @@ var questions = [
     },
 ];
 
-function preGame() {
-    //Display instructions and start button
-    $("#instructions").text(instructionsContent);
-    $("#instructions").show();
-    $("#start").show();
-    $(".gamePage").hide();
-    $(".resultsPage").hide();
+$(document).ready(function () {
+    function preGame() {
+        //Display instructions and start button
+        $("#instructions").text(instructionsContent);
+        $("#instructions").show();
+        $("#start").show();
+        $(".gamePage").hide();
+        $(".resultsPage").hide();
+    }
 
-    $("#start").click(function() {
+    $("#start").click(function () {
+        // clearInterval(timer)
+        // countDown = 10
         $(this).hide();
         $("#instructions").hide();
         startGame();
     })
-}
 
-function startGame() {
-    console.log("Game Started")
-    $(".gamePage").show();
-    startTimer();
-    showQuestions();
-}
-
-function showQuestions() {
-    for (let i = 0; i < questions.length; i++) {
-        var textDiv = $("<p>")
-        textDiv.text(questions[i].text);
-        $(".questionsDisplay").append(textDiv);
-
-        for (let j = 0; j < questions[i].choices.length; j++) {
-            var choicesDiv = $("<br><input type='radio' name=" + i + ">" + questions[i].choices[j] + "</input>")
-            $(textDiv).append(choicesDiv);
-        }
-    }
-};
-
-// set and countdown Timer
-function startTimer() {
-    timer = setInterval(function () {
-    countDown--;
-    $("#timerBox").text(countDown);
-    if (countDown === 0) {
-        clearInterval(timer);
-        clearInterval(countDown);
+    $("#submit").click(function () {
+        // clearInterval(timer)
         endGame();
-    };
-    },1000);
-}
-
-$("#correct").text("Correct: " + numCorrect);
-$("#incorrect").text("Incorrect: " + numIncorrect);
-$("#unanswered").text("Unanswered: " + numUnanswered)
-
-function endGame() {
-    $(".gamePage").hide();
-    $(".resultsPage").show();
-    $(".questionsDisplay").empty();
-    $("#restart").click(function(){
-        preGame();
     });
-}
 
-//Display Questions (at least 3)
-//Answer Options
-//Submit button 
+    function startGame() {
+        countDown = 10
+        $("#timerBox").text(countDown);
+        console.log("Game Started", countDown)
+        $(".gamePage").show();
+        startTimer();
+        $(".questionsDisplay").empty();
+        showQuestions();
+        
+    }
 
-//on Submit button click
-//end game--
-//display correct, incorrect, unanswered
-//try again button (cal start game function)
+    function showQuestions() {
+        for (let i = 0; i < questions.length; i++) {
+            var textDiv = $("<p>")
+            textDiv.text(questions[i].text);
+            $(".questionsDisplay").append(textDiv);
 
-$(document).ready(function(){
+            for (let j = 0; j < questions[i].choices.length; j++) {
+                var choicesDiv = $("<br><input type='radio'" + " val= " + j + " name=" + i + ">" + questions[i].choices[j] + "</input>")
+                $(textDiv).append(choicesDiv);
+            }
+        }
+    };
+
+    // set and countdown Timer
+    function startTimer() {
+        timer = setInterval(function () {
+            countDown--;
+            $("#timerBox").text(countDown);
+            if (countDown === 0) {
+                // clearInterval(timer);
+                // clearInterval(countDown);
+                endGame();
+            };
+        }, 1000);
+    }
+
+    function endGame() {
+        $(".gamePage").hide();
+        $(".resultsPage").show();
+        clearInterval(timer)
+        $("#restart").click(function () {
+            // countDown = 10;
+            preGame();
+            // startGame()
+        });
+    }
+
     preGame();
 
+
+    $("#correct").text("Correct: " + numCorrect);
+    $("#incorrect").text("Incorrect: " + numIncorrect);
+    $("#unanswered").text("Unanswered: " + numUnanswered);
 });
